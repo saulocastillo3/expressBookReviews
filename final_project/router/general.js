a7fefb16ca172e73db0540e7d2df9271310d6068
saulocastillo3 <saulocastillo3@gmail.com>
2024-08-19 15:50:10 -0400

    update1

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,51 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.send(JSON.stringify(books, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;  
+  return res.send(JSON.stringify(books[isbn], null, 4)); 
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+     
+    newBook ={}; 
+    const author = req.params.author; 
+    let i = 1; 
+    for (let isbn in books){
+        if(books[isbn].author===author){
+            newBook[i++] = books[isbn]; 
+        }
+    
+    }
+    res.send(JSON.stringify(newBook, null, 5));
+    
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    newBook ={}; 
+    const title = req.params.title; 
+    let i = 1; 
+    for (let isbn in books){
+        if(books[isbn].title===title){
+            newBook[i++] = books[isbn]; 
+        }
+    
+    }
+    res.send(JSON.stringify(newBook, null, 5));
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn; 
+  const reviews  = books[isbn].reviews 
+  return res.send(JSON.stringify(reviews, null, 4)); 
 });
 
 module.exports.general = public_users;
